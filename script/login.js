93aed5012c93ef4fd2662315f9742f64f4cdddda
alfian <alfiansakuragi@gmail.com>
2026-06-28 08:07:02 +0800

    4.3

--- script/login.js
+++ script/login.js
@@ -5,6 +5,17 @@ const registNow = document.querySelector(".register-link")
 const loginNow = document.querySelector(".Login-link")
 const loginBox = document.querySelector(".login-container")
 const registBox = document.querySelector(".register-container")
+const sidebar = document.querySelector(".sidebar")
+const hamburger = document.querySelector("#hamburger")
+const hamburgerClose = document.querySelector("#hamburger-close")
+const hamburgerLogin = document.querySelector("#hamburger-login")
+
+hamburger.addEventListener("click", () => {
+    sidebar.style.transform = "translateX(0)";
+})
+hamburgerClose.addEventListener("click", () => {
+    sidebar.style.transform = "translateX(100%)";
+})
 
 close[0].addEventListener("click", () => {
     loginRegister.style.display = "none"
@@ -13,6 +24,9 @@ close[1].addEventListener("click", () => {
     loginRegister.style.display = "none"
 })
 
+hamburgerLogin.addEventListener("click", () => {
+    loginRegister.style.display = "flex"
+})
 
 login.addEventListener("click", () => {
     loginRegister.style.display = "flex"
